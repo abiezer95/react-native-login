2986d6896635601f5913873663d635ee19b12547
abiezer <master21delakal@live.com>
2020-02-12 15:19:18 +0100

    testing de login completado login

--- _test_/home.test.js
+++ _test_/home.test.js
@@ -1,54 +1,19 @@
 import 'react-native';
-import React, {Component} from 'react';
-// import Home from '../App';
+import React from 'react';
 import regex from '../assets/testing_src/Regular_expresions_login';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+// 
 import Login from '../components/Login';
 import App from '../App'
 import SignUp from '../components/SignUp'
-// import { Component } from 'react';
 
-
-it('Login habdledInput', () => {
+it('Login submit', () => {
     const tree = renderer.create(<Login/>).getInstance()
-    expect(tree.handledInput({text: 'DATAS'})).toBe('DATAS')
-   
+    expect(tree.handledLoginPress()).toBe(true)
 })
-it('Login ccc', () => {
-    const tree = renderer.create(<Login/>).getInstance()
-    // expect(tree.handledInput({text: 'DATAS'})).toBe('DATAS')
-    expect(tree.handledLoginPress({text: 'DATAS'})).toBe('DATAS')
-    expect(tree.onAddTodo())
-   
-})
-// it('should render username input', () => {
-//     let tree = renderer.create(<Login />).toJSON()
 
-//     expect(global.utils.findById(tree, 'user')).toBeDefined()
-// })
 jest.useFakeTimers();
-
-// class homeTest extends Component {
-//     constructor(){
-//         super();
-//         this.state = {
-//             userName : "",
-//             Email : "",
-//             password : ""
-//         }
-//     }
-//     render() {
-//         return (
-//             regex.username.test('Login test user-name',()=>{
-//                 // <condition> ? <true> : <false>
-//                 // (regex.username === false) ? console.log('esta mal'): console.log('esta bien');
-//                 expect(regex.username.test('hola como')).toBe(true);
-//             })
-//         )
-//     }
-// }
-
-// export default homeTest;
 
 test('Login test user-name', () => {
     let HomeData = renderer.create(<Login />).getInstance();
@@ -58,33 +23,45 @@ test('Login test user-name', () => {
 });
 
 test('Login test email', () => {
-    
     // expect(HomeData.change(username)).toBe(10);
     // console.log(regex.email.test('abiezer95gmail.com'));
     expect(regex.email.test('ememem@gmail.com')).toBe(true);
 });
 
 test('Login test phoneNumber', () => {
-    
     // expect(HomeData.change(username)).toBe(10);
     // console.log(regex.email.test('abiezer95gmail.com'));
     expect(regex.phoneNumber.test('809-999-8587')).toBe(true);
 });
 
-test('<Login />',()=>{
+describe('<Login />',()=>{
     const login = renderer.create(<Login/>);
-    
     expect(login)
+
+    test('Login habdledInput function', () => {
+        expect(login.getInstance().handledInput('user', 'admin')).toBe('admin')
+        expect(login.getInstance().handledInput('password', '12345')).toBe('12345')
+    })
+
+    const component = shallow(<Login />);
+
+    test('Input user onchage function', () => {
+        component.find('#userName').simulate('change', {target: {value: 'admin'}});
+        expect(component.find('#userName').prop('value').target.value).toBe('admin');
+    })
+    
+    test('Input password onchage function', () => {
+        component.find('#password').simulate('change', {target: {value: '12345'}});
+        expect(component.find('#password').prop('value').target.value).toBe('12345');
+    })
 });
 
 test('<SignUp />',()=>{
     const sign = renderer.create(<SignUp />)
-
     expect(sign)
-    
 });
+
 test('<App />',()=>{
     const app = renderer.create(<App />);
-
     expect(app)
 });
